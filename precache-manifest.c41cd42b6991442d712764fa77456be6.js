eab67e0e7ce569db75750e41cae323d471ac0a3b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-27 18:34:01 -0500

    Updates

--- precache-manifest.c41cd42b6991442d712764fa77456be6.js
+++ precache-manifest.c41cd42b6991442d712764fa77456be6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc422d69cdc5ce3059e45425cb2e9f30",
+    "revision": "9e5f97fc5c6fa69b5923572119fd09bc",
     "url": "/video-app/index.html"
   },
   {
-    "revision": "fa4aeb9f72430baa64f6",
+    "revision": "1586dc09068f8598aaee",
     "url": "/video-app/static/css/main.a938a468.chunk.css"
   },
   {
-    "revision": "fda66bf63e34b8c2a01d",
-    "url": "/video-app/static/js/2.ff71ac33.chunk.js"
+    "revision": "6203f5910e4c6165eb77",
+    "url": "/video-app/static/js/2.65a0b71a.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/video-app/static/js/2.ff71ac33.chunk.js.LICENSE.txt"
+    "url": "/video-app/static/js/2.65a0b71a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fa4aeb9f72430baa64f6",
-    "url": "/video-app/static/js/main.98090415.chunk.js"
+    "revision": "1586dc09068f8598aaee",
+    "url": "/video-app/static/js/main.06328475.chunk.js"
   },
   {
     "revision": "3ff3d8e6424d94d15138",
